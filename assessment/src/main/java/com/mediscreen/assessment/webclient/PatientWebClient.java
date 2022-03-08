766f9ea25660e6caaca67ee9d6472d003d568a00
Achille Deribreux <achille@deribreux.be>
2022-03-08 07:06:09 +0100

    Webclients

---
+++ assessment/src/main/java/com/mediscreen/assessment/webclient/PatientWebClient.java
@@ -0,0 +1,32 @@
+package com.mediscreen.assessment.webclient;
+
+import com.mediscreen.assessment.dto.PatientDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+public class PatientWebClient {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String BASE_URL_LOCALHOST_PATIENT = "localhost:8081/patient";
+    private final String GET_PATIENT_BY_ID_URL = "/getById";
+    private final String QUERY_PARAM_PATIENT_ID = "?id=";
+
+
+    public List<PatientDTO> getAllNotesByPatientId(int patientId){
+        ResponseEntity<List<PatientDTO>> result = restTemplate.exchange(
+                BASE_URL_LOCALHOST_PATIENT+
+                        GET_PATIENT_BY_ID_URL+
+                        QUERY_PARAM_PATIENT_ID+patientId
+                , HttpMethod.GET,null,new ParameterizedTypeReference<List<PatientDTO>>() {});
+        return result.getBody();
+    }
+}
